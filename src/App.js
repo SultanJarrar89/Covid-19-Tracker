22102e5f1a7f5e7dc71d8329dfe529577966804a
sultan <sultanjarrar89@gmail.com>
2021-02-12 19:22:42 -0800

    project layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-d
 import './App.css'
 function App() {
   return (
     <div className='App'>
       <h1>Covid 19 Tracker</h1>
+      {/* Header */}
+
+      {/* Title + dropdown field */}
+      {/* InfoBox */}
+      {/* InfoBox */}
+      {/* InfoBox */}
+      {/* Map */}
     </div>
   )
 }
